d61389393de84b4761cdd71a4f628e8ed8232bbf
Jinnie <hjk7639@gmail.com>
2023-09-08 21:23:23 +0900

    The Coding Interview - 3. String Reversal

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// My version
+// function reverse(str) {
+//   let reversed = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+
+//   return reversed;
+// }
+
+// Solution #1
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// Solution #2
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// Solution #3
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => character + reversed, '');
+// }
 
 module.exports = reverse;
