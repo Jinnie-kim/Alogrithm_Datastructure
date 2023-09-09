482f10cdd4cbad8339dbd9720104f26b27524c25
Jinnie <hjk7639@gmail.com>
2023-09-09 09:54:53 +0900

    The Coding Interview - 4. Palindromes

--- AlgoCasts/exercises/palindrome/index.js
+++ AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,31 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// My version
+// function palindrome(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed === str;
+// }
+
+// Solution #1 - most easiest, kind a direct or the most compact one
+// function palindrome(str) {
+//   let reversed = str.split('').reverse().join('');
+
+//   return str === reversed;
+// }
+
+// Solution #3
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    console.log('char', char);
+    console.log('str[str.length - i -1]', str[str.length - i - 1]);
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
