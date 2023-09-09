124ee929399748cba4880f3a2074d47292051a06
Jinnie <hjk7639@gmail.com>
2023-09-09 11:19:33 +0900

    The Coding Interview - 6. MaxChar

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,36 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// My version
+// function maxChar(str) {
+//   let strObj = {};
+
+//   for (let char of str) {
+//     strObj[char] ? (strObj[char] = strObj[char] + 1) : (strObj[char] = 1);
+//   }
+
+//   return Object.keys(strObj).reduce((a, b) => (strObj[a] > strObj[b] ? a : b));
+// }
+
+// Solution #1
+function maxChar(str) {
+  const charMap = {};
+
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] ? (charMap[char] = charMap[char] + 1) : (charMap[char] = 1);
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
