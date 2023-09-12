9d2aa709a0dc9da5c60a9fcea70c51b833e37ade
Jinnie <hjk7639@gmail.com>
2023-09-12 11:34:45 +0900

    The Coding Interview - 9. Anagrams

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,65 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// My version
+// function anagrams(stringA, stringB) {
+//   const wordA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   const wordB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+//   if (wordA.length !== wordB.length) return false;
+
+//   const frequencyCounterA = {};
+//   const frequencyCounterB = {};
+
+//   for (let char of wordA) {
+//     frequencyCounterA[char] ? (frequencyCounterA[char] += 1) : (frequencyCounterA[char] = 1);
+//   }
+
+//   for (let char of wordB) {
+//     frequencyCounterB[char] ? (frequencyCounterB[char] += 1) : (frequencyCounterB[char] = 1);
+//   }
+
+//   for (let key in frequencyCounterA) {
+//     if (!(key in frequencyCounterB)) return false;
+//     if (!(frequencyCounterA[key] === frequencyCounterB[key])) return false;
+//   }
+
+//   return true;
+// }
+
+// Solution # 1.
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) return false;
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) return false;
+//   }
+
+//   return true;
+// }
+
+// // helper function
+// function buildCharMap(string) {
+//   const charMap = {};
+
+//   for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+// Solution # 2
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+// helper function
+function cleanString(string) {
+  return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
