498feac28df33259b9f2c241d435fc03ef906f0c
Jinnie <hjk7639@gmail.com>
2023-09-15 21:30:11 +0900

    The Coding Interview - 11. Printing Steps

--- AlgoCasts/exercises/steps/index.js
+++ AlgoCasts/exercises/steps/index.js
@@ -17,6 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// My Version
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let step = '#'.repeat(i) + ' '.repeat(n - i);
+//     console.log(step);
+//   }
+// }
+
+// Solution # 1 - iterate
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) stair += '#';
+//       else stair += ' ';
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// Solution # 2 - recursive
+function steps(n, row = 0, stair = '') {
+  if (n === row) return;
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // if (stair.length <= row) {
+  //   stair += '#';
+  // } else {
+  //   stair += ' ';
+  // }
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
