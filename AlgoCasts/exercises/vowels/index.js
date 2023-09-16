ee77e21d7583a6e847634fdca11015ceda115178
Jinnie <hjk7639@gmail.com>
2023-09-17 08:41:10 +0900

    The Coding Interview - 13. Find The Vowels

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,34 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// My version
+// const vowelsArr = ['a', 'e', 'i', 'o', 'u'];
+
+// function vowels(str) {
+//   let vowelCount = 0;
+
+//   for (let vowel of str) {
+//     if (vowelsArr.includes(vowel.toLowerCase())) vowelCount++;
+//   }
+
+//   return vowelCount;
+// }
+
+// Solution # 1. iterate
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) count++;
+//   }
+//   return count;
+// }
+
+// Solution # 2. regex
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
