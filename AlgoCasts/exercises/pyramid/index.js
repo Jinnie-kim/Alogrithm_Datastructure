434e075fd189f31032f6c4f680fa23236142d5fa
Jinnie <hjk7639@gmail.com>
2023-09-16 19:09:21 +0900

    The Coding Interview - 12. Two Sided Steps - Pyramids

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// q&a version
+// function pyramid(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(' '.repeat(n - i) + '#'.repeat(i * 2 - 1) + ' '.repeat(n - i));
+//   }
+// }
+
+// Solution # 1. iterate
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2); // midpoint index
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (midpoint - row <= col && midpoint + row >= col) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
+
+// Solution # 2. recursive
+function pyramid(n, row = 0, level = '') {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (n === row) return;
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
