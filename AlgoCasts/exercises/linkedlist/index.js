21c84a05ee4896415e3074ea60e39d5fb8e1d3f8
Jinnie <hjk7639@gmail.com>
2023-09-22 20:34:08 +0900

    The Coding Interview - 21. Linked Lists ~ insertFirst

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -2,8 +2,24 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+// const list = new LinkedList();
+// list.head = new Node(10);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+}
 
 module.exports = { Node, LinkedList };
