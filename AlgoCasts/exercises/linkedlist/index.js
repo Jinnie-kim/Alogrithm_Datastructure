aab3c6966319666816a458bb4658574c94d1175d
Jinnie <hjk7639@gmail.com>
2023-09-23 11:23:03 +0900

    The Coding Interview - 21. Linked Lists - insertAt

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -117,6 +117,19 @@ class LinkedList {
 
     prevNode.next = prevNode.next.next;
   }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    const prevNode = this.getAt(index - 1) || this.getLast();
+    prevNode.next = new Node(data, prevNode.next);
+  }
 }
 
 module.exports = { Node, LinkedList };
