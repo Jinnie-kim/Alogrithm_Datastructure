9362dd86124881e79c6d0e830e681a2ad26fc3b0
Jinnie <hjk7639@gmail.com>
2023-09-28 20:54:56 +0900

    The Coding Interview - 30. Back to JS: Events

--- AlgoCasts/exercises/events/index.js
+++ AlgoCasts/exercises/events/index.js
@@ -4,16 +4,34 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const callbackFns = this.events[eventName];
+    if (callbackFns) {
+      for (callbackFn of callbackFns) {
+        callbackFn();
+      }
+    } else return;
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
