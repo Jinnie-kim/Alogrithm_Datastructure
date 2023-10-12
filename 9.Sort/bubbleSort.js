68985536ef0c4ac6f8db2c30bf882652c9931e37
Jinnie <hjk7639@gmail.com>
2023-10-12 15:21:11 +0900

    Sort - Bubble Sort

---
+++ 9.Sort/bubbleSort.js
@@ -0,0 +1,33 @@
+// Bubble Sort
+// O(n * 2)
+function bubbleSortNaive(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+function bubbleSort(arr) {
+  let noSwaps = true;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+
+  return arr;
+}
