068a06385fd5790f814a99c4bef129e4112cfc39
Jinnie <hjk7639@gmail.com>
2023-10-18 08:39:03 +0900

    Sort - Radix Sort : change parameter name

--- 9.Sort/radixSort.js
+++ 9.Sort/radixSort.js
@@ -7,32 +7,32 @@ function digitCount(num) {
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
-function mostDigits(nums) {
+function mostDigits(numsArr) {
   let maxDigits = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  for (let i = 0; i < numsArr.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(numsArr[i]));
   }
 
   return maxDigits;
 }
 
-function radixSort(nums) {
-  let maxDigitsCount = mostDigits(nums);
+function radixSort(numsArr) {
+  let maxDigitsCount = mostDigits(numsArr);
 
   for (let k = 0; k < maxDigitsCount; k++) {
     let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < nums.length; i++) {
-      let digit = getDigit(nums[i], k);
-      digitBuckets[digit].push(nums[i]);
+    for (let i = 0; i < numsArr.length; i++) {
+      let digit = getDigit(numsArr[i], k);
+      digitBuckets[digit].push(numsArr[i]);
     }
     console.log(digitBuckets);
-    nums = [].concat(...digitBuckets);
-    console.log(nums);
+    numsArr = [].concat(...digitBuckets);
+    console.log(numsArr);
     console.log('--------------------------');
   }
 
-  return nums;
+  return numsArr;
 }
 
 radixSort([23, 345, 5467, 12, 2345, 9852]);
