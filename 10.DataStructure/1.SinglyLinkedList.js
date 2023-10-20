bff8a5c6cb68face55a157e97592650eaf9f8aa7
Jinnie <hjk7639@gmail.com>
2023-10-20 09:25:19 +0900

    SinglyLinkedList - unshift

--- 10.DataStructure/1.SinglyLinkedList.js
+++ 10.DataStructure/1.SinglyLinkedList.js
@@ -68,6 +68,22 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
