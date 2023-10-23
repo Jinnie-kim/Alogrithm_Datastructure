19c569d830ad632a3eda166929c380a245caef26
Jinnie <hjk7639@gmail.com>
2023-10-23 09:04:31 +0900

    Stack - Implementing a stack using linked list

--- 11.Stacks&Queues/1.Stack.js
+++ 11.Stacks&Queues/1.Stack.js
@@ -16,3 +16,50 @@ stack2.unshift('create new file');
 stack2.unshift('resized file');
 
 stack2.shift(); // 'resized file'
+
+// Linked List implementation
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let oldFirst = this.first;
+      this.first = newNode;
+      this.first.next = oldFirst;
+    }
+
+    this.length++;
+
+    return this.length;
+  }
+
+  pop() {
+    if (!this.first) return null;
+
+    let deletedNode = this.first;
+    if (this.length === 1) {
+      this.last = null;
+    }
+    this.first = this.first.next; // this.first to be null as well.
+
+    this.length--;
+
+    return deletedNode.value;
+  }
+}
