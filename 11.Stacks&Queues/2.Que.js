48f2d76cc23b290fca21d9db0f790a1c81a8116e
Jinnie <hjk7639@gmail.com>
2023-10-25 08:59:35 +0900

    Que - Implementing a que structure using an array

---
+++ 11.Stacks&Queues/2.Que.js
@@ -0,0 +1,16 @@
+// Implementation using Array
+
+let q = [];
+
+q.push('First');
+q.push('Second');
+q.push('Third');
+
+q.shift(); // 'First'
+
+// Or...
+q.unshift('First');
+q.unshift('Second');
+q.unshift('Third');
+
+q.pop(); // 'First'
