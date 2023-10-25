5865f77fa0e67570cdea0640157546a790e574e3
Jinnie <hjk7639@gmail.com>
2023-10-25 09:00:07 +0900

    Que - Implementing a que structure using a linkedlist

--- 11.Stacks&Queues/2.Que.js
+++ 11.Stacks&Queues/2.Que.js
@@ -14,3 +14,46 @@ q.unshift('Second');
 q.unshift('Third');
 
 q.pop(); // 'First'
+
+// implementation using Linked List
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    let deleteNode = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+
+    return deleteNode.value;
+  }
+}
